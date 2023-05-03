5624c5c8420a716a7a08775dfd9af9c8ab40a4e6
Timothy Fraser <timothy.fraser.1@gmail.com>
2023-05-03 13:43:35 -0400

    ---

--- R/test.R
+++ R/test.R
@@ -1,1 +1,13 @@
-#
+#' @name test
+#' @title Title of test() Function
+#' @description Description
+#' @param parameter1 stuff
+#' @param parameter2 more stuff
+#' @export
+
+test = function(parameter1, parameter2){
+  
+  
+  
+
+}
