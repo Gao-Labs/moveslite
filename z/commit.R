c5b0a92b5fc33ba1cf7289f320be5893f93202d5
Timothy Fraser <timothy.fraser.1@gmail.com>
2023-05-03 13:18:39 -0400

    ---

--- z/commit.R
+++ z/commit.R
@@ -3,6 +3,9 @@
 # This script commits to github.
 require(gert)
 
+# Add paths of files you want to **commit** to Github
 gert::git_add(dir(all.files = TRUE))
+# Commit all those changed files
 gert::git_commit_all(message = "---")
+# Push those files to Github
 gert::git_push()
