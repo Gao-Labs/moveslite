a2913c15d63f4f3eddf077a2ffbff17774035c83
Timothy Fraser <timothy.fraser.1@gmail.com>
2023-05-12 16:35:22 -0400

    ---

--- R/estimate.R
+++ R/estimate.R
@@ -2,33 +2,40 @@
 #' @title Estimate your area-specific model
 #' @param .filters (vector/list) Named Vector or List of inputted values for filtering.
 #' @param .vars (character vector) Vector of variables to use as predictors.
-#' @param .check (logical) Check the R2 of the model and print warning if below 95%? (TRUE/FALSE)
 #' @importFrom dplyr %>% filter tbl
 #' @importFrom broom glance
 #' @importFrom stats lm
 #' @note An example filter would be: `.filters = c(.by = 8, .pollutant = 98, .sourcetype = 42)`
 #' @export
 
-estimate = function(data, .vars = c("vmt", "vehicles", "starts", "sourcehours", "year"), .check = FALSE){
+estimate = function(data, .vars = c("vmt", "vehicles", "starts", "sourcehours", "year")){
 
   ####################################
   # EDITS NEEDED
   ###################################
   # -- Do a few versions of this model to account for different numbers of .vars
-  
+
+  # Ideas:
+  # .type
+
+  # 10 models that make plausible sense that try to maximize R2
+
+  # switch(
+  #   EXPR = .type,
+  #
+  #   "1" = { m = data %>% lm(formula = emissions ~ vmt + vehicles + starts + sourcehours + year)   },
+  #
+  #   "2" = { m = data %>% lm(formula = emissions ~ poly(vmt, 2) + vehicles + starts + sourcehours + year)  },
+  #
+  #   "3" = { m = data %>% lm(formula = emissions ~ poly(vmt, 3) + vehicles + starts + sourcehours + year)  },
+  #
+  #   "4" = {  m = data %>% lm(formula = emissions ~ vmt + year) }
+  #   )
+
   # Compute the model
   m = data %>% lm(formula = emissions ~ poly(vmt, 3) + vehicles + starts + sourcehours + year)
-  
-  # If Diagnostic checks are desired, will evaluate
-  if(.check == TRUE){
-    # Use broom to view the statistics
-    stat = broom::glance(m)
-    # Make a warning if the model is not very accurate.
-    if(stat$r.squared < 0.95){ print("Warning: Model is < 95% accurate. R2 = ", round(stat$r.squared, 3) ) }
-  }
-  
-  
+
   # Return model.
   return(m)
-  
+
 }
